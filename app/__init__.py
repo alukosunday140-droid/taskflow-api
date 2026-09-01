94635d6bf286d67d5a3a6b714a273fc9eee03326
alukosunday140-droid <alukosunday140@gmail.com>
2026-09-01 18:09:20 +0100

    refractor: use development configuration by default

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-from app.config import Config
+from app.config import DevelopmentConfig
 from app.extensions import db
 
 
-def create_app(config_class=Config):
+def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
